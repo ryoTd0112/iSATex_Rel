8f9102c5b4f2aae0168eee6b8348f6c0e3ccbbac
Ryo Tandai <ryo.s1042@gmail.com>
2021-03-07 11:43:02 +0900

    fix: docs

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -3,7 +3,7 @@ var DOCUMENTATION_OPTIONS = {
     VERSION: '0.0.5',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
-    BUILDER: 'singlehtml',
+    BUILDER: 'html',
     FILE_SUFFIX: '.html',
     LINK_SUFFIX: '.html',
     HAS_SOURCE: true,
